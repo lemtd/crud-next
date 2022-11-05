fee29876396020f4f34790a2097ca122867142d1
Leticia Deodato <leticiamanuelletd@gmail.com>
2022-11-05 19:19:30 -0300

    Alteração da dimensão do layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
     return (
         <div className={`
-        flex flex-col w-2/3 bg-white text-gray-500 rounded-md`}>
+        flex flex-col w-100 bg-white text-gray-500 rounded-md`}>
             <Title>{props.title}</Title>
             <div className="p-7">
                 {props.children}
